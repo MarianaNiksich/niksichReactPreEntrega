0fe8ad8670988e5e2bcf4fee4a3528a897776cf9
MarianaNiksich <niksichmariana01@gmail.com>
2023-09-06 18:20:16 -0300

    arreglando productsdata

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import ItemListContainer from "../components/ItemListContainer";
 import { productsData } from "../JSON/AsyncMock"
 const Home = () => {
     return (
-        <ItemListContainer titulo="¡Bienvenidos a Book Storage!" parrafo="¡Descubre un mundo de conocimiento en nuestra librería online! Encuentra los mejores libros en todas las categorías imaginables y sumérgete en historias cautivadoras, aprendizaje fascinante y descubrimientos asombrosos." />
+        <ItemListContainer productsData={productsData} />
     )
 
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,79 +3,30 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 
-const productsData = [
-    {
-        "id": 1,
-        "titulo": "Cuentos completos de Lorrie Moore",
-        "autor": "Lorrie Moore",
-        "genero": "literatura",
-        "precio": 2500,
-        "stock": 3,
-        "img": "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQXPuukZq2TyPkVgDCLeryOMjXVrAqlQtyd2YKTPo_CrDMqfBue-ZgqFIgLookph__EM5yS1jmtdAq_TJrZg71MUSL-G8gWeGMA56efSNc3UvNdwCQJ2JI5&usqp=CAc"
-    },
-    {
-        "id": 2,
-        "titulo": "Las palabras justas",
-        "autor": "Milena Busquets",
-        "genero": "literatura",
-        "precio": 3000,
-        "stock": 2,
-        "img": "https://images.cdn2.buscalibre.com/fit-in/360x360/ec/21/ec212322c4d5525a2c171f96b81e34a1.jpg"
-    },
-    {
-        "id": 3,
-        "titulo": "La teoria de los archipiélagos",
-        "autor": "Alice Keller",
-        "genero": "literatura",
-        "precio": 4000,
-        "stock": 5,
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFLsDF1kDUwR_eclKYnnjnoTqBebG_Qa4yig&usqp=CAU"
-    },
-    {
-        "id": 4,
-        "titulo": "Diccionario Holandés",
-        "autor": "Libreía universitaria",
-        "genero": "idiomas",
-        "precio": 2000,
-        "stock": 1,
-        "img": "https://images.cdn3.buscalibre.com/fit-in/360x360/83/31/8331f9e126460e6d32b7fa4bcc599460.jpg"
-    },
-    {
-        "id": 5,
-        "titulo": "Ready for B2 first",
-        "autor": "Macmillan education",
-        "genero": "idiomas",
-        "precio": 5500,
-        "stock": 6,
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzlyK7U7HO3jec84RoDnHnbqWmn-igrWNvhhcBaM-xRuzE2KXpsXrQleUEVAW9uCqeO-I&usqp=CAU"
-    },
-    {
-        "id": 6,
-        "titulo": "Rayuela",
-        "autor": "Julio Cortazar",
-        "genero": "literatura",
-        "precio": 3000,
-        "stock": 7,
-        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUvGJ2BsLuor-IJIoZIJ1Ku94m224nvj7j8A&usqp=CAU"
-    }
-]
+
 function ItemListContainer({ productsData }) {
-    console.log(productsData)
-    productsData.map((item) => {
-        return (
-            <>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={item.imagen} />
-                    <Card.Body>
-                        <Card.Title>{item.titulo}</Card.Title>
-                        <Card.Text>
-                            {item.autor}
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </>
-        )
-    })
+
+    return (
+        <div>
+            {productsData.map((item) => {
+                return (
+                    <>
+
+                        <Card style={{ width: '18rem' }} key={item.id}>
+                            <Card.Img variant="top" src={item.img} />
+                            <Card.Body>
+                                <Card.Title>{item.titulo}</Card.Title>
+                                <Card.Text>
+                                    {item.autor}
+                                </Card.Text>
+                                <Button variant="primary">Ir al detalle</Button>
+                            </Card.Body>
+                        </Card>
+                    </>
+                )
+            })
+            }
+        </div>
+    )
 }
 export default ItemListContainer
